b8362438b10853753aa4e3fdb93111c3a092ec92
IndigoCarmine <79552713+IndigoCarmine@users.noreply.github.com>
2023-09-05 20:14:15 +0900

    remove an unnecessary flag

--- Core/Src/led.c
+++ Core/Src/led.c
@@ -6,16 +6,15 @@ struct led_status
     GPIO_TypeDef *gpio;
     uint16_t pin;
     uint32_t before_tick;
-    uint8_t is_high;
 };
 
 #define LED_LIST_SIZE 4
 struct led_status led_list[] =
     {
-        {LED_CAN_GPIO_Port, LED_CAN_Pin, 0, 0},
-        {LED_GREEN_GPIO_Port, LED_GREEN_Pin, 0, 0},
-        {LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, 0, 0},
-        {LED_RED_GPIO_Port, LED_RED_Pin, 0, 0},
+        {LED_CAN_GPIO_Port, LED_CAN_Pin, 0},
+        {LED_GREEN_GPIO_Port, LED_GREEN_Pin, 0},
+        {LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, 0},
+        {LED_RED_GPIO_Port, LED_RED_Pin, 0},
 };
 
 void led_on(led name)
@@ -32,10 +31,9 @@ void led_process(void)
 {
     for (int i = 0; i < LED_LIST_SIZE; i++)
     {
-        if (led_list[i].is_high || HAL_GetTick() - led_list[i].before_tick > LED_INTERVAL)
+        if (HAL_GetTick() - led_list[i].before_tick > LED_INTERVAL)
         {
             HAL_GPIO_WritePin(led_list[i].gpio, led_list[i].pin, GPIO_PIN_RESET);
-            led_list[i].is_high = 0;
         }
     }
 }
